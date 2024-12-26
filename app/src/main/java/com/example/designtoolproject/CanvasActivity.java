e26d4b5207e6b35685439a28e7025713306830c3
Almondd17 <116758564+Almondd17@users.noreply.github.com>
2024-12-26 13:24:05 +0200

    better navigation view design and a complete drawing mode select menu

--- app/src/main/java/com/example/designtoolproject/CanvasActivity.java
+++ app/src/main/java/com/example/designtoolproject/CanvasActivity.java
@@ -1,11 +1,16 @@
 package com.example.designtoolproject;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.widget.FrameLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class CanvasActivity extends AppCompatActivity {
 private BottomNavigationView optionMenu;
@@ -19,7 +24,20 @@ private CanvasView canvasView;
         optionMenu = findViewById(R.id.optionMenu);
 
         //set selected menu item listeners and change selected drawing mode
-        //optionMenu.setOnItemSelectedListener();
+        optionMenu.setOnItemSelectedListener(item -> {
+            Map<Integer, String> idModeMap = new HashMap<>();
+            idModeMap.put(R.id.pencil, "pencil");
+            idModeMap.put(R.id.brush, "brush");
+            idModeMap.put(R.id.circle, "circle");
+            idModeMap.put(R.id.rectangle, "rectangle");
+            idModeMap.put(R.id.line, "line");
 
+            String mode = idModeMap.get(item.getItemId());
+            if (mode != null) {
+                canvasView.setMode(mode);
+            }
+
+            return true;
+        });
     }
 }
